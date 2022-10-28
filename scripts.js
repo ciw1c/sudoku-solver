a90569a98bf3ff54470bf27ff49fd16db580864a
ciw1c <imciwic@gmail.com>
2022-10-29 00:59:12 +0200

    Add generator, downloader and deleted keyboard input

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ function init_sudoku() {
     tr = document.createElement('tr');
     cols = 0;
     for (var j=0; j<9; j++) {
-      number = "<td><input type='text' class='input-number' data-row='" + i + "' data-col='" + j + "' maxlength=1 onkeypress='sudoku_digits(event)'></td>"
+      number = "<td><input type='text' class='input-number' data-row='" + i + "' data-col='" + j + "' maxlength=1' readonly></td>"
       $(tr).append(number);
       if (cols % 3 == 0) {
         $(tr).find('td').last().addClass('left-border-strong');
@@ -20,33 +20,5 @@ function init_sudoku() {
       $(tr).addClass('top-border-strong');
     rows++;
     sudoku_tbody.append(tr);
-  }
-  
-  $('.input-number').on('change', function() {
-    apply_delete(this);
-  });
-}
-
-function apply_delete(elem) {
-  if ($(elem).val() == '') {
-    let i = $(elem).data('row');
-    let j = $(elem).data('col');
-    let n = sudoku[i][j];
-    update_aux_structures(n-1,i,j,true);
-    sudoku[i][j] = 0;
-  }
-}
-
-function sudoku_digits(event) {
-  if (event.key < '1' || event.key > '9') 
-    event.preventDefault();
-  else {
-    let i = $(event.target).data('row');
-    let j = $(event.target).data('col');
-    let n = event.keyCode - '0'.charCodeAt();
-    if (!is_valid_digit(i,j,n)) 
-      event.preventDefault();
-    else
-      $(event.target).val('');
   }
 }
--- sudoku.js
+++ sudoku.js
@@ -69,7 +69,7 @@ function solve_sudoku() {
   if (solve_sudoku_rec(0,0))
     print_sudoku();
   else
-    console.log('NO SOLUTION FOR THIS SUDOKU');
+    console.log('NO SOLUTION');
 }
 
 function is_possible(i,j,n) {
@@ -112,6 +112,41 @@ function solve_sudoku_rec(i,j) {
   return false;
 }
 
+function solve_sudoku_random_rec(i,j) {
+  // Buscamos la siguiente posicion a rellenar
+  let i_next = i;
+  let j_next = j + 1;
+  if (j_next == SIZE) {
+    i_next = i + 1;
+    j_next = 0;
+  }
+  
+  // Terminamos el SUUUdoku
+  if (i == SIZE)
+    return true;
+  
+  // Si estamos en posicion fija en el sudoku original, devolvemos siguiente pos
+  if (is_fixed(i,j) || sudoku[i][j] != 0)
+    return solve_sudoku_random_rec(i_next,j_next);
+
+  let random_list = Array.from(Array(SIZE), (n, index) => index);
+  random_list.sort(() => (Math.random() > .5) ? 1 : -1);
+  
+  // Probamos los números posibles
+  for (let k=0; k<SIZE; k++) {
+    let n = random_list[k];
+    if (is_possible(i,j,n)) {
+      sudoku[i][j] = (n+1);
+      update_aux_structures(n,i,j,false);
+      if (solve_sudoku_random_rec(i_next,j_next))
+        return true;
+      sudoku[i][j] = 0;
+      update_aux_structures(n,i,j,true);
+    }
+  }
+  return false;
+}
+
 function is_valid_digit(i,j,n) {
   if (n < 1 || n > 9)
     return false;
@@ -127,8 +162,67 @@ function is_valid_digit(i,j,n) {
 }
 
 function random_generate() {
-  //  Rellenar el sudoku con números aleatorios que tenga solución
-  //  Generar lista de todas las casillas
-  //  Mezclar lista de casillas
-  //  Vamos sacando casillas y eliminando el número si no es posible poner ningún otro más que el que está
+  original_sudoku = Array(SIZE).fill().map(()=>Array(SIZE).fill(0));
+  reset_sudoku();
+  solve_sudoku_random_rec(0,0);
+  let random_cells = [];
+  for (let i=0; i<SIZE; i++) {
+    for (let j=0; j<SIZE; j++) {
+      random_cells.push([i,j]);
+    }
+  }
+  random_cells.sort(() => (Math.random() > .5) ? 1 : -1);
+  for (let k=0; k<random_cells.length; k++) {
+    let i = random_cells[k][0];
+    let j = random_cells[k][1];
+    let n = sudoku[i][j];
+    
+    let able_delete = true;
+    for (let kk=0; kk<SIZE; kk++) {
+      if (kk == n-1) continue;
+      if (is_possible(i,j,kk)) {
+        able_delete = false;
+        break;
+      }
+    }
+
+    if (able_delete) {
+      update_aux_structures(n-1,i,j,true);
+      sudoku[i][j] = 0;
+    }
+  }
+  for (let i=0; i<SIZE; i++)
+    for (let j=0; j<SIZE; j++)
+      original_sudoku[i][j] = sudoku[i][j];
+  print_sudoku();
+}
+
+function sudoku_to_txt() {
+  sudoku_txt = '';
+  for (let i=0; i<SIZE; i++) {
+    for (let j=0; j<SIZE; j++) {
+      if (sudoku[i][j] == 0) 
+        sudoku_txt = sudoku_txt + '-';
+      else
+        sudoku_txt = sudoku_txt + sudoku[i][j];
+    }
+    sudoku_txt = sudoku_txt + '\n';
+  }
+  return sudoku_txt;
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+function download_sudoku() {
+  var text = sudoku_to_txt();
+  var filename = "sudoku.txt";
+  download(filename, text);
 }
